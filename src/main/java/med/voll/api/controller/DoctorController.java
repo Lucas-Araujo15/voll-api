ab3d38ad482396fc897f6cdbb056808d336306a8
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-08 16:38:05 -0300

    DELETE Requests

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -36,4 +36,10 @@ public class DoctorController {
         Doctor doctor = doctorRepository.getReferenceById(data.id());
         doctor.updateInformation(data);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable Long id) {
+        doctorRepository.deleteById(id);
+    }
 }
