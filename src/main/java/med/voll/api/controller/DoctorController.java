0eb87dd5a9ff16428d0efef03384dddd0b936862
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-08 14:57:29 -0300

    data production in API

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -3,13 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.doctor.Doctor;
+import med.voll.api.doctor.DoctorListData;
 import med.voll.api.doctor.DoctorRegisterData;
 import med.voll.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctor")
@@ -21,5 +22,10 @@ public class DoctorController {
     @Transactional
     public void create(@RequestBody @Valid DoctorRegisterData data) {
         doctorRepository.save(new Doctor(data));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DoctorListData>> list() {
+        return ResponseEntity.ok(doctorRepository.findAll().stream().map(DoctorListData::new).toList());
     }
 }
---
+++ src/main/java/med/voll/api/doctor/DoctorListData.java
@@ -0,0 +1,7 @@
+package med.voll.api.doctor;
+
+public record DoctorListData(String name, String email, String crm, Specialty specialty) {
+    public DoctorListData(Doctor doctor) {
+        this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
+    }
+}
