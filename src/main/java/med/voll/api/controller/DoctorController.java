26cea9549a8334a8726fd7bbc6b703bfd3473b63
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-11 14:13:10 -0300

    standardizing API returns

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -21,26 +21,31 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void create(@RequestBody @Valid DoctorRegisterData data) {
-        doctorRepository.save(new Doctor(data));
+    public ResponseEntity create(@RequestBody @Valid DoctorRegisterData data) {
+        doctorRepository.save(new Doctor(data);
     }
 
     @GetMapping
     public ResponseEntity<Page<DoctorListData>> list(@PageableDefault(size = 10, page = 0, sort = "name", direction = Sort.Direction.ASC) Pageable pagination) {
-        return ResponseEntity.ok(doctorRepository.findAllByActiveTrue(pagination).map(DoctorListData::new));
+        Page<DoctorListData> page = doctorRepository.findAllByActiveTrue(pagination).map(DoctorListData::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid DoctorUpdateData data) {
+    public ResponseEntity<DetailedDoctorData> update(@RequestBody @Valid DoctorUpdateData data) {
         Doctor doctor = doctorRepository.getReferenceById(data.id());
         doctor.updateInformation(data);
+
+        return ResponseEntity.ok(new DetailedDoctorData(doctor));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         Doctor doctor = doctorRepository.getReferenceById(id);
         doctor.delete();
+
+        return ResponseEntity.noContent().build();
     }
 }
