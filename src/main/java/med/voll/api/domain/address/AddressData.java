6a885d0371968700a0b082dd605e92ee2ea49725
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-14 16:48:54 -0300

    add spring security

--- src/main/java/med/voll/api/domain/address/AddressData.java
+++ src/main/java/med/voll/api/domain/address/AddressData.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record AddressData(
-        @NotBlank
+        @NotBlank(message = "{address.street}")
         String street,
 
         @NotBlank
