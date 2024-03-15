dca0b1cfabd0b944fbd1ce68c03660c1f7cca322
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-15 13:18:08 -0300

    refactor token generation

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.user.AuthenticationData;
 import med.voll.api.domain.user.User;
+import med.voll.api.infra.TokenJwtData;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,11 @@ public class AuthenticationController {
 
     @PostMapping
     public ResponseEntity<?> login(@RequestBody @Valid AuthenticationData data) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        Authentication authentication = manager.authenticate(token);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        Authentication authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok(tokenService.generateToken((User) authentication.getPrincipal()));
+        String tokenJWT = tokenService.generateToken((User) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJwtData(tokenJWT));
     }
 }
