2d4c19e3460c94b7ec57f27a17e2cb5f86ac8753
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-03-18 13:19:28 -0300

    integrity validation

--- src/main/java/med/voll/api/domain/appointment/AppointmentScheduling.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentScheduling.java
@@ -1,6 +1,6 @@
 package med.voll.api.domain.appointment;
 
-import jakarta.validation.Valid;
+import med.voll.api.domain.ValidationException;
 import med.voll.api.domain.doctor.Doctor;
 import med.voll.api.domain.doctor.DoctorRepository;
 import med.voll.api.domain.patient.Patient;
@@ -18,11 +18,22 @@ public class AppointmentScheduling {
     private PatientRepository patientRepository;
 
     public void schedule(AppointmentSchedulingData data) {
-        Doctor doctor = doctorRepository.findById(data.doctorId()).get();
-        Patient patient = patientRepository.findById(data.patientId()).get();
+        if (data.doctorId() != null && !doctorRepository.existsById(data.doctorId())) {
+            throw new ValidationException("ID do médico informado não existe");
+        }
 
+        if (!patientRepository.existsById(data.patientId())) {
+            throw new ValidationException("ID do paciente informado não existe");
+        }
+
+        Doctor doctor = chooseDoctor(data);
+        Patient patient = patientRepository.findById(data.patientId()).get();
 
         Appointment appointment = new Appointment(null, doctor, patient, data.date());
         appointmentRepository.save(appointment);
+    }
+
+    private Doctor chooseDoctor(AppointmentSchedulingData data) {
+
     }
 }
